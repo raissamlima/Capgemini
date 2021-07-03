f5a0b46a1ff0132ad864f31ab1a89549f947b40b
raissamlima <86810509+raissamlima@users.noreply.github.com>
2021-07-03 17:04:56 -0300

    Cadastrando itens

--- calculadora.js
+++ calculadora.js
@@ -1,25 +1,70 @@
-function calculaNumeroDeCliques(numeroDeVisualizacoes) {
-    return numeroDeVisualizacoes * 12 / 100;
-}
-
-function calculaNumeroDeCompartilhamentos(numeroDeCliques) {
-    return numeroDeCliques * 3 / 100;
-}
+var anuncios = new Array();
 
 function calculaNumeroDeVisualizacoes(numeroDeCompartilhamentos) {
     return numeroDeCompartilhamentos * 40;
 }
 
-function calculaValorPago(numeroDeVisualizacoes) {
-    return numeroDeVisualizacoes / 30;
-}
-
 function calcularAlcance(valorPago) {
-    var numeroDeVisualizacoes = valorPago * 30; 
-    var numeroDeCliques = calculaNumeroDeCliques(numeroDeVisualizacoes);
-    var numeroDeCompartilhamentos = calculaNumeroDeCompartilhamentos(numeroDeCliques);
+    var numeroDeVisualizacoes  =  valorPago  *  30 ; 
+    var numeroDeCompartilhamentos = calcularCompartilhamento(valorPago);
     var numeroDeVisualizacoesCompartilhados = calculaNumeroDeVisualizacoes(numeroDeCompartilhamentos);
     return numeroDeVisualizacoes + (numeroDeVisualizacoesCompartilhados * 4);
+}
+
+function calcularCompartilhamento(valorPago) {
+    var numeroDeCliques = calcularCliques(valorPago);
+    return numeroDeCliques * 3 / 100;
+}
+
+function calcularCliques(valorPago) {
+    var numeroDeVisualizacoes = valorPago * 30; 
+    return numeroDeVisualizacoes * 12 / 100;
+}
+
+function cadastrar() {
+    let form = document.getElementById('cadastro');
+    
+    let nome = form.elements['nome'].value;
+    let cliente = form.elements['cliente'].value;
+    let dataInicio = new Date(form.elements['data-inicio'].value);
+    let dataFinal = form.elements['data-final'].value;
+    let investimento = form.elements['investimento'].value;
+
+    let visualizacoes = calcularAlcance(investimento);
+    let compartilhamentos = calcularCompartilhamento(investimento);
+    let cliques = calcularCliques(investimento);
+
+    var anuncio = {
+        nome: nome,
+        cliente: cliente,
+        dataInicio: dataInicio,
+        dataFinal: dataFinal,
+        investimento: investimento,
+        visualizacoes: visualizacoes,
+        compartilhamentos: compartilhamentos,
+        cliques: cliques
+    };
+
+    anuncios.push(anuncio);
+    atualizaLista();
+}
+
+function atualizaLista() {
+
+    let lista = document.getElementById('lista-conteudo');
+    var html = "";
+
+    for(var i = 0; i < anuncios.length; i++) {
+        let anuncioHtml = gerarAnuncioHtml(anuncios[i]);
+        html += anuncioHtml;
+    }
+
+    lista.innerHTML = html;
+}
+
+function gerarAnuncioHtml(anuncio) {
+    let anuncioHtml = "<tr><td>" + anuncio.nome + "</td><td>" + anuncio.cliente + "</td><td>" + anuncio.investimento + "</td><td>" + anuncio.visualizacoes + "</td><td>" + anuncio.cliques + "</td><td>" + anuncio.compartilhamentos + "</td></tr>";
+    return anuncioHtml;
 }
 
 var teste = calcularAlcance(7.40); 
