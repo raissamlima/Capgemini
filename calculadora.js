6cc70a32b12c2d88655150ddfa41fc6b24c94a5e
raissamlima <86810509+raissamlima@users.noreply.github.com>
2021-07-03 18:25:24 -0300

    Separando CSS, validando datas, filtrando por nome

--- calculadora.js
+++ calculadora.js
@@ -21,35 +21,86 @@ function calcularCliques(valorPago) {
     return numeroDeVisualizacoes * 12 / 100;
 }
 
+function quantidadeDeDias(dataInicio, dataFinal) {
+    let tempo = Math.abs(dataFinal - dataInicio);
+    let dias = Math.ceil(tempo / (1000 * 60 * 60 * 24)) + 1;
+    return dias;
+}
+
 function cadastrar() {
     let form = document.getElementById('cadastro');
     
     let nome = form.elements['nome'].value;
     let cliente = form.elements['cliente'].value;
-    let dataInicio = new Date(form.elements['data-inicio'].value);
-    let dataFinal = form.elements['data-final'].value;
+    let dataInicio = Date.parse(form.elements['data-inicio'].value);
+    let dataFinal = Date.parse(form.elements['data-final'].value);
     let investimento = form.elements['investimento'].value;
 
-    let visualizacoes = calcularAlcance(investimento);
-    let compartilhamentos = calcularCompartilhamento(investimento);
-    let cliques = calcularCliques(investimento);
+    if(nome.length == 0 || cliente.length == 0 || investimento.length == 0) {
+        alert("Preencha todos os campos!");
+        return
+    }
+
+    if(isNaN(dataInicio) || isNaN(dataFinal)) {
+        alert("Data inválida!");
+        return
+    } 
+
+    if(dataInicio > dataFinal) {
+        alert("A data de inicio deve ser menor ou igual a data final");
+        return
+    }
+
+    let totalDeDias = quantidadeDeDias(dataInicio, dataFinal);
+    let valorTotal = totalDeDias * investimento;
+    let visualizacoes = calcularAlcance(valorTotal);
+    let compartilhamentos = calcularCompartilhamento(valorTotal);
+    let cliques = calcularCliques(valorTotal);
 
     var anuncio = {
         nome: nome,
         cliente: cliente,
         dataInicio: dataInicio,
         dataFinal: dataFinal,
-        investimento: investimento,
+        investimento: valorTotal,
         visualizacoes: visualizacoes,
         compartilhamentos: compartilhamentos,
         cliques: cliques
     };
 
     anuncios.push(anuncio);
-    atualizaLista();
+
+    form.reset();
+    atualizaLista(anuncios);
 }
 
-function atualizaLista() {
+function filtrar() {
+    let form = document.getElementById('filtro');
+    
+    let cliente = form.elements['cliente'].value;
+    let dataInicio = Date.parse(form.elements['data-inicio'].value);
+    let dataFinal = Date.parse(form.elements['data-final'].value);
+
+    if(!isNaN(dataInicio) && !isNaN(dataFinal)) {
+        if(dataInicio > dataFinal) {
+            alert("A data de inicio deve ser menor ou igual a data final");
+            return
+        }
+    } 
+
+    var listaFiltrada = new Array();
+
+    for(var i = 0; i < anuncios.length; i++) {
+        if(anuncios[i].cliente.includes(cliente)){
+            listaFiltrada.push(anuncios[i]);
+        }
+    }
+
+    atualizaLista(listaFiltrada);
+
+}
+
+function atualizaLista(anuncios) {
 
     let lista = document.getElementById('lista-conteudo');
     var html = "";
